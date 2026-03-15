1933a30b6f75a41423d98455eb987587bb632efd
Dayer <dayerxr.cher@gmail.com>
2026-03-15 13:45:02 +0800

    feat: added form tags

--- src/components/chat/input.tsx
+++ src/components/chat/input.tsx
@@ -5,17 +5,17 @@ import { Button } from '@/components/ui/button'
 export default function ChatInput(){
 
     return(
-        <div className="max-w-2xl w-6/10 border-4 rounded-2xl p-4 mx-auto my-auto">
+        <form action="" method="POST" className="max-w-2xl w-6/10 border-4 rounded-2xl p-4 mx-auto my-auto">
             <Textarea className="resize-none border-0 ring-0 focus-visible:ring-0 focus-visible:border-0 focus-visible:ring-offset-0 shadow-none" 
             placeholder="How can I help you today?" />
             <div className="flex justify-between p-2">
                 <Button variant="outline">
                     <Plus />
                 </Button>
-                <Button>
+                <Button type="submit">
                     <ArrowUp />
                 </Button>
             </div>
-        </div>
+        </form>
     )
 }
